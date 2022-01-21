fc8cafc08da48e301eb07cf3e421f55b50478cff
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-01-21 21:12:12 +0100

    Make Mobile phone unique

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,7 +53,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'phone' => ['required', 'numeric', 'min:11']
+            'phone' => ['required', 'numeric', 'min:11','unique:users'],
         ]);
     }
 
