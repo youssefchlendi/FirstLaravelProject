ad25e147c805c49c745107398cee2148d32f949e
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-01-24 16:47:15 +0100

    Test

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
+    //hamza
     protected $table="students";
     protected $primaryKey="id";
     protected $fillable=['student_name','course','fee'];
